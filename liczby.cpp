7b936ba42fe80d75e488d4ded1311e056214b253
resttoo <dododatkowych@gmail.com>
2017-03-26 13:55:15 +0200

    Update liczby.cpp

--- liczby.cpp
+++ liczby.cpp
@@ -6,7 +6,29 @@ bool compare(const int& a, const int& b)
 {
         return a>b;
 }
+
 void sortuj()
+{
+
+    ifstream input("nazwiska.txt");
+    ofstream output("nazwiska_leksyko.txt");
+
+    list<string> nazwisko;
+    string dane;
+
+    do{
+       input>>dane;
+       nazwisko.push_back(dane);
+
+    }while(!input.eof());
+
+    nazwisko.sort(compare);
+
+    for (list<string>::iterator it=nazwisko.begin(); it!=nazwisko.end() ; ++it)
+    output << *it <<endl;
+
+}
+void sortuj1()
 {
 
     ifstream input("liczby.txt");
@@ -31,5 +53,6 @@ void sortuj()
 int main()
 {
     sortuj();
+    sortuj1();
     return 0;
 }
